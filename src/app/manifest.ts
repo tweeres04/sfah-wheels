dca2865de917aa73e7c337839360af90105917e2
Tyler Weeres <tweeres04@gmail.com>
2024-04-27 18:02:56 -0700

    Fix favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,20 +1,21 @@
 import { MetadataRoute } from 'next'
- 
+import icon from './icon.png'
+
 export default function manifest(): MetadataRoute.Manifest {
-  return {
-    name: 'SFAH Wheels',
-    short_name: 'SFAH Wheels',
-    description: 'Learn what ingredients go together',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
-    icons: [
-      {
-        src: '/cooking-pot.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-  }
+	return {
+		name: 'SFAH Wheels',
+		short_name: 'SFAH Wheels',
+		description: 'Learn what ingredients go together',
+		start_url: '/',
+		display: 'standalone',
+		background_color: '#fff',
+		theme_color: '#fff',
+		icons: [
+			{
+				src: icon.src,
+				sizes: '512x512',
+				type: 'image/png',
+			},
+		],
+	}
 }
